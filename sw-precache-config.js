08b933c7b809bb27a82670791d337f140a79586b
Chris Joel <cdata@google.com>
2016-09-16 18:16:49 -0700

    Update polymer cli configuration. Still broken, though.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,11 +3,8 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.js',
-    '/bower_components/web-animations-js/web-animations.min.js',
     '/bower_components/webrtc-polyfill/index.js',
-    '/src/images/*',
-    '/src/images/polymon/*',
-    '/src/polymon-app/polymon.json'
+    '/images/polymon_sprite_sheet.svg'
   ],
   navigateFallback: '/index.html'
 };
