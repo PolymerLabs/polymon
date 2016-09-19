534a339a3713e2009cd9017b5d4b4dd5a7e4abf0
Chris Joel <cdata@google.com>
2016-09-18 17:31:41 -0700

    Add icon and theme to Web App Manifest

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,6 +5,7 @@ module.exports = {
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
     '/bower_components/webrtc-polyfill/index.js',
     '/images/polymon_sprite_sheet.svg',
+    '/images/polymon_icon.png',
     '/fonts/**/*'
   ],
   navigateFallback: '/index.html',
